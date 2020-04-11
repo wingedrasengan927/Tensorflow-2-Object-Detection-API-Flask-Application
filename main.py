c0d88b9a09c8e50f4501ea9e8f907658eb507dd4
wingedrasengan927 <34920015+wingedrasengan927@users.noreply.github.com>
2020-04-11 11:44:28 -0700

    Add argument parser

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from flask import Flask, request, Response
 import numpy as np
 import cv2
 import json
+import argparse
 
 from utilities import load_model, load_label_map, show_inference, parse_output_dict
 from custom_np_encoder import NumpyArrayEncoder
+
+DEFAULT_PORT = 8004
+DEFAULT_HOST = '127.0.0.1'
 
 model_path = "models/ssd_mobilenet_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03/saved_model"
 labels_path = "data/mscoco_label_map.pbtxt"
@@ -14,6 +18,25 @@ max_boxes = 20
 
 detection_model = load_model(model_path)
 category_index = load_label_map(labels_path)
+
+def parse_args():
+
+  parser = argparse.ArgumentParser(description='Tensorflow object detection API')
+
+  parser.add_argument('--debug', dest='debug',
+                        help='Run in debug mode.',
+                        required=False, action='store_true', default=False)
+
+  parser.add_argument('--port', dest='port',
+                        help='Port to run on.', type=int,
+                        required=False, default=DEFAULT_PORT)
+
+  parser.add_argument('--host', dest='host',
+                        help='Host to run on, set to 0.0.0.0 for remote access', type=str,
+                        required=False, default=DEFAULT_HOST)
+
+  args = parser.parse_args()
+  return args
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -47,4 +70,6 @@ def infer():
 
 
 # start flask app
-app.run(host="localhost", port=5000, debug=True)
+def main():
+  args = parse_args()
+  app.run(host=args.host, port=args.port, debug=args.debug)
